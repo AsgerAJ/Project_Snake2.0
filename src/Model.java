0c51183fd11f122117afd440bb8ed403f893c702
AsgerAJ <asger@allin.dk>
2024-01-03 19:15:05 +0100

    Scaling somewhat implemented

--- src/Model.java
+++ src/Model.java
@@ -23,23 +23,24 @@ public class Model extends Application {
     private Circle food;
     private Rectangle snake;
     private Direction direction;
-    private Line gridLine;
-    private Rectangle bg;
-
-    public static int step = 10;
-    public static int n = 0;
-    public static int m = 0;
-    public static Scene scene;
+    private Rectangle back;
+    public double scalingConstant;
+    public double positioncorrection;
 
     public static void main(String[] args) {
         launch(args);
-        boolean alive = true;
     }
 
     public void drawGrid(int x, int y) {
+        if(x>y){
+            scalingConstant = 500/(x/10);
+        }else{
+            scalingConstant = 500/(y/10);
+        }
+
         for (int i = 0; i < x / 10; i++) {
-            for (int j = 0; j < y; j++) {
-                Rectangle back = new Rectangle(i * 10, j * 10, 10, 10);
+            for (int j = 0; j < y / 10; j++) {
+                Rectangle back = new Rectangle(i*scalingConstant, j*scalingConstant, scalingConstant, scalingConstant);
                 if (((i % 2 == 0) && (j % 2 == 0)) || ((i % 2 != 0) && (j % 2 != 0))) {
                     back.setFill(Color.LIGHTGREEN);
                 } else {
@@ -50,74 +51,48 @@ public class Model extends Application {
         }
     }
 
-    public void newFood(int x, int y) {
-        Circle food = new Circle(x - 5, y - 5, 5);
+    public void newFood(double x, double y) {
+        System.out.println(x + ", " + y);
+        x = x * 10-(scalingConstant/10);
+        y = y * 10-(scalingConstant/10);
+        
+        positioncorrection = (scalingConstant/(scalingConstant/(scalingConstant/10)));
+        Circle food = new Circle(x*positioncorrection, y*positioncorrection, scalingConstant/2);
         food.setFill(Color.RED);
         root.getChildren().add(food);
     }
 
-    public void newSnake(int x, int y, int length) {
+    public void newSnake(int x, int y) {
         x = x / 2;
         y = y / 2;
-        ArrayList<Rectangle> snake = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            snake.add(new Rectangle(x, y + (i * 10), 10, 10));
-            snake.get(i).setFill(Color.BLACK);
-            root.getChildren().add(snake.get(i));
-        }
+        snake = new Rectangle(x, y, 10, 10);
+        snake.setFill(Color.BLACK);
+        root.getChildren().add(snake);
+        // ArrayList<Rectangle> snake = new ArrayList<>();
+        // snake.add(new Rectangle(x, y, 10, 10));
+        // snake.add(new Rectangle(x, y + 10, 10, 10));
+        // for (int i = 0; i < 2; i++) {
+        // snake.get(i).setFill(Color.BLACK);
+        // root.getChildren().add(snake.get(i));
+        // }
     }
 
-    public void updateSnake(Direction direction) {
-        switch (direction) {
-            case Up:
-                if (snake.getY() - step < 0) {
-                    snake.setY(m - 10);
-                } else {
-                    snake.setY(snake.getY() - step);
-                }
+    public void updateSnake() {
 
-                break;
-
-            case Left:
-                if (snake.getX() - step < 0) {
-                    snake.setX(n - 10);
-                } else {
-                    snake.setX(snake.getX() - step);
-                }
-                break;
-
-            case Right:
-                if (snake.getX() + step > n - 10) {
-                    snake.setX(0);
-                } else {
-                    snake.setX(snake.getX() + step);
-                }
-                break;
-
-            case Down:
-                if (snake.getY() + step > m - 10) {
-                    snake.setY(0);
-                } else {
-                    snake.setY(snake.getY() + step);
-                }
-                break;
-
-            default:
-                break;
-        }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scanner console = new Scanner(System.in);
 
-        System.out.println("Please enter the dimensions of the snake game: (n x m)");
-        System.out.print("n = ");
-        int n = 500;// console.nextInt()*10;
-        System.out.println();
-        System.out.print("m = ");
-        int m = 500;// console.nextInt()*10;
+        //System.out.println("Please enter the dimensions of the snake game: (n x m)");
+        //System.out.print("n = ");
+        int n = 9 * 10; // console.nextInt() * 10;
+        //System.out.println();
+        //System.out.print("m = ");
+        int m = 9 * 10; //console.nextInt() * 10;
         int radius = 10;
+        int step = 10;
 
         root = new Pane();
         root.setPrefSize(n, m);
@@ -125,29 +100,53 @@ public class Model extends Application {
         direction = Direction.Left;
 
         drawGrid(n, m);
-        newFood(20, 20);
-        newSnake(n, m, 1);
+        System.out.println(scalingConstant);
+        newFood(rand.nextInt(n/10)+1, rand.nextInt(m/10)+1);
+        System.out.println(positioncorrection);
+        // newSnake(n, m);
         Scene scene = new Scene(root, 500, 500);
 
-        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            KeyCode input = event.getCode();
-            switch (input) {
-                case W:
-                    direction = Direction.Up;
-                    break;
-                case A:
-                    direction = Direction.Left;
-                    break;
-                case S:
-                    direction = Direction.Down;
-                    break;
-                case D:
-                    direction = Direction.Right;
-                    break;
-                default:
-                    break;
-            }
-        });
+        // scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+        // KeyCode code = event.getCode();
+        // switch (code) {
+        // case UP:
+        // if (snake.getY() - step < 0) {
+        // snake.setY(m-10);
+        // } else {
+        // snake.setY(snake.getY() - step);
+        // }
+
+        // break;
+
+        // case LEFT:
+        // if (snake.getX() - step < 0) {
+        // snake.setX(n-10);
+        // } else {
+        // snake.setX(snake.getX() - step);
+        // }
+        // break;
+
+        // case RIGHT:
+        // if (snake.getX() + step > n-10) {
+        // snake.setX(0);
+        // } else {
+        // snake.setX(snake.getX() + step);
+        // }
+        // break;
+
+        // case DOWN:
+        // if (snake.getY() + step > m-10) {
+        // snake.setY(0);
+        // } else {
+        // snake.setY(snake.getY() + step);
+        // }
+        // break;
+
+        // default:
+        // break;
+        // }
+        // });
+
         primaryStage.setTitle("Snake");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
---
+++ src/Snake.java
@@ -0,0 +1,13 @@
+import javafx.scene.shape.Rectangle;
+import java.util.*;
+
+import org.w3c.dom.css.Rect;
+
+public class Snake extends Rectangle {
+    
+
+    private List<Rectangle> Snake;
+    private int snakeLength = 2;
+    private Direction currentDirection;
+    
+}
