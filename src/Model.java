4661148d34f76f07b36f56255fa9ca788747664a
AsgerAJ <asger@allin.dk>
2024-01-03 22:11:07 +0100

    Rough draft af snake movement

--- src/Model.java
+++ src/Model.java
@@ -21,9 +21,10 @@ public class Model extends Application {
 
     private Pane root;
     private Circle food;
-    private Rectangle snake;
+    private Snake snake;
     private Direction direction;
     private Rectangle back;
+
     public double scalingConstant;
     public double positioncorrection;
 
@@ -31,20 +32,20 @@ public class Model extends Application {
         launch(args);
     }
 
-    public void drawGrid(int x, int y) {
-        if(x>y){
-            scalingConstant = 500/(x/10);
-        }else{
-            scalingConstant = 500/(y/10);
+    public void drawGrid(int x, int y) { // Colours background
+        if (x > y) {
+            scalingConstant = 500 / (x);
+        } else {
+            scalingConstant = 500 / (y);
         }
-
-        for (int i = 0; i < x / 10; i++) {
-            for (int j = 0; j < y / 10; j++) {
-                Rectangle back = new Rectangle(i*scalingConstant, j*scalingConstant, scalingConstant, scalingConstant);
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                Rectangle back = new Rectangle(i * scalingConstant, j * scalingConstant, scalingConstant,
+                        scalingConstant);
                 if (((i % 2 == 0) && (j % 2 == 0)) || ((i % 2 != 0) && (j % 2 != 0))) {
-                    back.setFill(Color.LIGHTGREEN);
+                    back.setFill(Color.YELLOWGREEN);
                 } else {
-                    back.setFill(Color.GREEN);
+                    back.setFill(Color.DARKSEAGREEN);
                 }
                 root.getChildren().add(back);
             }
@@ -53,44 +54,29 @@ public class Model extends Application {
 
     public void newFood(double x, double y) {
         System.out.println(x + ", " + y);
-        x = x * 10-(scalingConstant/10);
-        y = y * 10-(scalingConstant/10);
-        
-        positioncorrection = (scalingConstant/(scalingConstant/(scalingConstant/10)));
-        Circle food = new Circle(x*positioncorrection, y*positioncorrection, scalingConstant/2);
+        Circle food = new Circle(x * scalingConstant - scalingConstant / 2, y * scalingConstant - scalingConstant / 2,
+                scalingConstant / 2);
         food.setFill(Color.RED);
         root.getChildren().add(food);
     }
 
     public void newSnake(int x, int y) {
-        x = x / 2;
-        y = y / 2;
-        snake = new Rectangle(x, y, 10, 10);
-        snake.setFill(Color.BLACK);
+        System.out.println();
+        snake = new Snake(x, y, scalingConstant, 2, Direction.Up);
         root.getChildren().add(snake);
-        // ArrayList<Rectangle> snake = new ArrayList<>();
-        // snake.add(new Rectangle(x, y, 10, 10));
-        // snake.add(new Rectangle(x, y + 10, 10, 10));
-        // for (int i = 0; i < 2; i++) {
-        // snake.get(i).setFill(Color.BLACK);
-        // root.getChildren().add(snake.get(i));
-        // }
-    }
-
-    public void updateSnake() {
-
+        snake.eat();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scanner console = new Scanner(System.in);
 
-        //System.out.println("Please enter the dimensions of the snake game: (n x m)");
-        //System.out.print("n = ");
-        int n = 9 * 10; // console.nextInt() * 10;
-        //System.out.println();
-        //System.out.print("m = ");
-        int m = 9 * 10; //console.nextInt() * 10;
+        // System.out.println("Please enter the dimensions of the snake game: (n x m)");
+        // System.out.print("n = ");
+        int n = 50; // console.nextInt();
+        // System.out.println();
+        // System.out.print("m = ");
+        int m = 50; // console.nextInt();
         int radius = 10;
         int step = 10;
 
@@ -101,51 +87,45 @@ public class Model extends Application {
 
         drawGrid(n, m);
         System.out.println(scalingConstant);
-        newFood(rand.nextInt(n/10)+1, rand.nextInt(m/10)+1);
+        newFood(2, 2);
         System.out.println(positioncorrection);
-        // newSnake(n, m);
+        newSnake(n, m);
+
         Scene scene = new Scene(root, 500, 500);
 
-        // scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-        // KeyCode code = event.getCode();
-        // switch (code) {
-        // case UP:
-        // if (snake.getY() - step < 0) {
-        // snake.setY(m-10);
-        // } else {
-        // snake.setY(snake.getY() - step);
-        // }
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            KeyCode code = event.getCode();
+            switch (code) {
+                case UP:
+                    if (snake.getCurrDir() != Direction.Down) {
+                        snake.setCurrentDirection(Direction.Up);
 
-        // break;
+                    }
+                    break;
+                case DOWN:
+                    if (snake.getCurrDir() != Direction.Up) {
+                        snake.setCurrentDirection(Direction.Down);
+                    }
+                    break;
+                case LEFT:
+                    if (snake.getCurrDir() != Direction.Right) {
+                        snake.setCurrentDirection(Direction.Left);
+                    }
+                    break;
+                case RIGHT:
+                    if (snake.getCurrDir() != Direction.Left) {
+                        snake.setCurrentDirection(Direction.Right);
+                    }
+                    break;
 
-        // case LEFT:
-        // if (snake.getX() - step < 0) {
-        // snake.setX(n-10);
-        // } else {
-        // snake.setX(snake.getX() - step);
-        // }
-        // break;
-
-        // case RIGHT:
-        // if (snake.getX() + step > n-10) {
-        // snake.setX(0);
-        // } else {
-        // snake.setX(snake.getX() + step);
-        // }
-        // break;
-
-        // case DOWN:
-        // if (snake.getY() + step > m-10) {
-        // snake.setY(0);
-        // } else {
-        // snake.setY(snake.getY() + step);
-        // }
-        // break;
-
-        // default:
-        // break;
-        // }
-        // });
+                case SPACE:
+                    snake.update(snake.getCurrDir());
+                    System.out.println(snake.getCurrDir());
+                        break;
+                default:
+                    break;
+            }
+        });
 
         primaryStage.setTitle("Snake");
         primaryStage.setScene(scene);
--- src/Snake.java
+++ src/Snake.java
@@ -1,13 +1,82 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.scene.input.*;
+
 import java.util.*;
 
-import org.w3c.dom.css.Rect;
-
 public class Snake extends Rectangle {
-    
 
-    private List<Rectangle> Snake;
+    private ArrayList<Rectangle> body;
     private int snakeLength = 2;
     private Direction currentDirection;
-    
+    private Rectangle head;
+    private double cS;
+    private int xLim;
+    private int yLim;
+
+    public Snake(int x, int y, double cS, int snakeLength, Direction currentDirection) {
+        super((x / 2) * cS - cS, ((y / 2)) * cS - cS,cS, cS);
+        this.cS = cS;
+        this.xLim = x;
+        this.yLim = y;
+        this.body = new ArrayList<>();
+        this.currentDirection = Direction.Up;
+    }
+
+    public void eat() {
+        System.out.println("Nom nom nom");
+    }
+
+    public void setCurrentDirection(Direction currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public Direction getCurrDir(){
+        return this.currentDirection;
+    }
+
+    public int getXLim(){
+        return this.xLim;
+    }
+
+    public int getYLim(){
+        return this.yLim;
+    }
+
+    public void update(Direction direction){
+        switch (direction) {
+            case Up:
+                if(getY() - cS < 0){
+                    setY(getYLim()*cS);
+                }else{
+                    setY(getY() - cS);
+                }
+                break;
+            
+            case Left:
+                setX(getX() - cS);
+                break;
+
+            case Right:
+                setX(getX() + cS);
+                break;
+
+            case Down:
+                setY(getY() + cS);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
